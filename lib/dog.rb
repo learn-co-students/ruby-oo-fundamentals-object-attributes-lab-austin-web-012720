da52fd087359bdeaef066d9f8aa59e1ded7ec4ee
jacqueshebert <jacques.henri.hebert@gmail.com>
2020-01-28 12:12:52 -0600

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+    
+
+    def breed
+        @breed
+    end
+    
+    def breed=(b)
+        @breed = b
+    end
+
+    def name
+        @name
+    end
+    
+    def name=(n)
+        @name = n
+    end
+end
